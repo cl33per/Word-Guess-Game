81e06746d7b13b1c57dfeabef58e39db8e073c25
cl33per <leeper08@gmail.com>
2019-08-20 09:32:30 -0400

    js works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,26 @@
-$(document).ready(function (){
 
-    var wordToMatch
-    game.resetgame
-    
+// Globabl Variables
+var numGuess
+var wordToMatch
+
+// Game Object
 var game = {
+    wins: 0,
+    maxGuess: 10,
+    pauseGame: false,
+    guessedLetters: [],
+    gussingWord: [],
     possibleWords: ["florence", "paris", "madrid", "rome", "singapore", "dubai", "new york city",
         "shanghai", "london", "tokyo", "sydney", "toronto", "beijing", "moscow",
         "johannesburg", "istanbul", "warsaw", "jakarta", "kuala lumpur", "mexico city",
         "hong kong", "chicago", "seoul", "los angeles", "mumbai"
     ],
-    guessedLetters: [],
-    guessingWord: [],
-    pausegame: false,
-    numGuess: 10,
-    wins: 0,
-    maxGuess: 10,
-    isAlpha: function (ch) {
+    // Checks if the letter pressis is A-Z
+    isLetter: function (ch) {
         return /^[A-Z]$/i.test(ch);
     },
-    updateDisplay: function () {
-        document.getElementById("totalWins").innerText = this.wins
-        document.getElementById("currentWord").innerText = this.guessingWord.join("")
-        document.getElementById("remainingGuesses").innerText = this.numGuess
-        document.getElementById("guessedLetters").innerText = this.guessedLetters.join(" ")
-    },
-
-    resetgame: function () {
-        this.numGuess = 10
-        this.pausegame = false
-
-        // Get a new word
-        wordToMatch = this.possibleWords[Math.floor(Math.random() * this.possibleWords.length)].toUpperCase()
-        console.log(wordToMatch)
-
-        // Reset word arrays
-        this.guessedLetters = []
-        this.guessingWord = []
-
-        // Reset the guessed word
-        for (var i = 0, j = wordToMatch.length; i < j; i++) {
-            // Put a space instead of an underscore between multi word "words"
-            if (wordToMatch[i] === " ") {
-                this.guessingWord.push(" ")
-            } else {
-                this.guessingWord.push("_")
-            }
-        }
-
-        // Update the Display
-        this.dateDisplay()
-    },
-    checkForLetter: function (letter) {
+    // This is the next method to check if the letter is in the word
+    letterCheck: function (letter) {
         var foundLetter = false
         var correctSound = document.createElement("audio")
         var incorrectSound = document.createElement("audio")
@@ -60,47 +30,76 @@ var game = {
         // Search string for letter
         for (var i = 0, j = wordToMatch.length; i < j; i++) {
             if (letter === wordToMatch[i]) {
-                this.guessingWord[i] = letter
+                game.guessingWord[i] = letter
                 foundLetter = true
                 correctSound.play()
-                // If guessing word matches random word
-                if (this.guessingWord.join("") === wordToMatch) {
+                // logic for word guesssed to match wordToMatch
+                if (game.guessingWord.join("") === wordToMatch) {
                     // Increment # of wins
-                    this.wins++
-                    this.pausegame = true
-                    this.updateDisplay()
-                    setTimeout(this.resetgame, 5000)
+                    game.wins++
+                    game.pauseGame = true
+                    game.updateDisplay()
+                    setTimeout(game.resetGame, 5000)
                 }
             }
         }
 
         if (!foundLetter) {
             incorrectSound.play()
-            // Check if inccorrect guess is already on the list
-            if (!this.guessedLetters.includes(letter)) {
-                // Add incorrect letter to guessed letter list
-                this.guessedLetters.push(letter)
-                // Decrement the number of remaining guesses
-                this.numGuess--
+            // Checks the array for duplicates
+            if (!game.guessedLetters.includes(letter)) {
+                game.guessedLetters.push(letter)
+                numGuess--
             }
-            if (this.numGuess === 0) {
-                // Display word before reseting game
-                this.guessingWord = this.wordToMatch.split()
-                this.pausegame = true
-                setTimeout(this.resetgame, 5000)
+            if (numGuess === 0) {
+                // Updates the display when the game is over showing the word
+                game.guessingWord = wordToMatch.split()
+                game.pauseGame = true
+                setTimeout(game.resetGame, 5000)
             }
         }
 
-        this.updateDisplay()
+        game.updateDisplay()
 
+    },
+    resetGame: function () {
+        numGuess = game.maxGuess
+        game.pauseGame = false
+
+        // Get a new word
+        wordToMatch = game.possibleWords[Math.floor(Math.random() * game.possibleWords.length)].toUpperCase()
+        console.log(wordToMatch)
+
+        // Array reset
+        game.guessedLetters = []
+        game.guessingWord = []
+
+        // Reset guessingword
+        for (var i = 0, j = wordToMatch.length; i < j; i++) {
+            // Put a space instead of an underscore between multi word "words"
+            if (wordToMatch[i] === " ") {
+                game.guessingWord.push(" ")
+            } else {
+                game.guessingWord.push("_")
+            }
+        }
+        game.updateDisplay()
+    },
+    updateDisplay: function () {
+        document.getElementById("totalWins").innerText = game.wins
+        document.getElementById("currentWord").innerText = game.guessingWord.join(" ")
+        document.getElementById("remainingGuesses").innerText = numGuess
+        document.getElementById("guessedLetters").innerText = game.guessedLetters.join(" ")
     }
 }
 
+$(document).ready(function () {
+game.resetGame();
 // Wait for key press
 document.onkeypress = function (event) {
     // Make sure key pressed is an alpha character
-    if (game.isAlpha(event.key) && !game.pausegame) {
-        game.checkForLetter(event.key.toUpperCase())
+    if (game.isLetter(event.key) && !game.pauseGame) {
+        game.letterCheck(event.key.toUpperCase())
     }
 }
-});
+})
